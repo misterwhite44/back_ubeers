e8c4f8afe0d1203420f4556722aa7def81859bd3
louisboucard <louis44.boucard@gmail.com>
2025-04-29 09:22:06 +0200

    remise du localhost, commentaire pour le front en ligne

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from mysql.connector import Error
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "https://ubeer-jade.vercel.app"}})
-#CORS(app, resources={r"/*": {"origins": "https://localhost:3000"}})
+#CORS(app, resources={r"/*": {"origins": "https://ubeer-jade.vercel.app"}})
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 api = Api(app, version='1.0', title='Ubeers API',
           description='API for managing beers, breweries, deliveries, and users')
 
