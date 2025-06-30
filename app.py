bfc993d4a5a3931a41ab08949ad70063242f5b0c
Tomate <louis44.boucard@gmail.com>
2025-06-30 13:05:48 +0200

    ee

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ from flask_cors import CORS
 from models import db
 from routes import api as routes_namespace
 import pymysql
-
+import os
+from dotenv import load_dotenv
 pymysql.install_as_MySQLdb()
+
+load_dotenv() 
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     "mysql+pymysql://uuw6sv5bvs11qa51:md1HTCoAHEg0s4HbJHGc@bwawmx4ntfjwzxxrotz4-mysql.services.clever-cloud.com:3306/bwawmx4ntfjwzxxrotz4?charset=utf8mb4"
 )
 
-CORS(app, origins=["https://ubeer-jade.vercel.app"])
+CORS(app, origins=[os.getenv("FRONT_URL")])
 
 db.init_app(app)
 routes_namespace.init_app(app)
