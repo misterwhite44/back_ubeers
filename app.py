8280a2c63c48ddcd9dea979f3992caf3da361b2a
Tomate <louis44.boucard@gmail.com>
2025-06-30 11:21:33 +0200

    modif url

--- app.py
+++ app.py
@@ -13,13 +13,12 @@ from routes.users import register_user_routes
 from routes.deliveries import register_delivery_routes
 
 app = Flask(__name__)
-#CORS(app, resources={r"/*": {"origins": "https://ubeer-jade.vercel.app"}})
-CORS(app, resources={r"/*": {"origins": "https://ubeer-jade.vercel.app"}})
-
+front_url = os.getenv("FRONT_URL")
+CORS(app, resources={r"/*": {"origins": front_url}})
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'https://ubeer-jade.vercel.app')
+    response.headers.add('Access-Control-Allow-Origin', front_url)
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
