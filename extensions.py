a6353742e82db962d0bb30784f1710ec4199fe71
Tomate <louis44.boucard@gmail.com>
2025-06-30 13:54:58 +0200

    zz

--- extensions.py
+++ extensions.py
@@ -4,9 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+redis_host = os.getenv("REDIS_HOST")
+redis_port = os.getenv("REDIS_PORT")
+redis_db = os.getenv("REDIS_DB")
+
+if not redis_host or not redis_port or not redis_db:
+    raise RuntimeError("Les variables REDIS_HOST, REDIS_PORT et REDIS_DB doivent être définies")
+
 redis_client = redis.Redis(
-    host=os.getenv("REDIS_HOST"),
-    port=int(os.getenv("REDIS_PORT")),
-    db=int(os.getenv("REDIS_DB")),
+    host=redis_host,
+    port=int(redis_port),
+    db=int(redis_db),
     decode_responses=True
 )
