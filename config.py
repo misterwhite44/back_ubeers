3311ec42a798a0d6ce3bf4c05dc43e3aafd06966
Tomate <louis44.boucard@gmail.com>
2025-06-30 08:16:20 +0200

    url

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+    REDIS_HOST = os.getenv("REDIS_HOST", "https://ubeer-jade.vercel.app")
     REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
     REDIS_DB = int(os.getenv("REDIS_DB", 0))
