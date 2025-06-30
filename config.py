34febbfe79e1a7cd8ac3cde05566a7daee26c4d4
Tomate <louis44.boucard@gmail.com>
2025-06-30 12:00:52 +0200

    ajout bdd classique

--- config.py
+++ config.py
@@ -2,9 +2,20 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 front_url = os.getenv("FRONT_URL")
 
 class Config:
     REDIS_HOST = os.getenv("REDIS_HOST", front_url)
     REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
     REDIS_DB = int(os.getenv("REDIS_DB", 0))
+
+    DB_CONFIG = {
+        'host': os.getenv("DB_HOST"),
+        'port': int(os.getenv("DB_PORT", 3306)),
+        'user': os.getenv("DB_USER"),
+        'password': os.getenv("DB_PASSWORD"),
+        'database': os.getenv("DB_NAME"),
+        'charset': os.getenv("DB_CHARSET", 'utf8mb4')
+}
+
